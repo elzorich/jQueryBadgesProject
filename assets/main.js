4c6e329ec8a6abb9700e70ffdf514dd9195351f0
e.zorich <emerald16>
2016-12-10 13:17:46 +0300

    adding ajax call

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,33 @@
-$(function() {
+$(function () {
 
-  // your code will go here
-
+    $.ajax({
+        url: 'https://www.codeschool.com/users/1252290.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            var arr = response.courses.completed;
+            for (var i = 0; i < arr.length; i++) {
+                var h3 = $("<h3 />", {
+                        text: arr[i].title
+                    }),
+                    img = $("<img />", {
+                        src: arr[i].badge
+                    }),
+                    a = $("<a />", {
+                        href: arr[i].url,
+                        "class": "btn btn-primary",
+                        target: "_blank",
+                        text:"See Course"
+                    });
+                $('#badges').append(
+                    $('<div/>')
+                        .addClass("course")
+                        .append(h3)
+                        .append(img)
+                        .append(a)
+                );
+            }
+        }
+    });
 });
+
+var httpRequest = new XMLHttpRequest();
